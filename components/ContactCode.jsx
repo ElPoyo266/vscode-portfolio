5b3e8b98afaf9ae94fd2da17030e3671db86a5f2
ElPoyo12 <pfournel@orange.fr>
2025-03-12 12:08:31 +0100

    Page contacte et page home OK

--- components/ContactCode.jsx
+++ components/ContactCode.jsx
@@ -2,75 +2,44 @@ import styles from '../styles/ContactCode.module.css';
 
 const contactItems = [
   {
-    social: 'website',
-    link: 'nitinranganath.me',
-    href: 'https://nitinranganath.me',
-  },
-  {
     social: 'email',
-    link: 'nitinranganath@gmail.com',
-    href: 'mailto:nitinranganath@gmail.com',
+    link: '"pfournel@orange.fr"',
+    href: 'mailto:pfournel@orange.fr',
   },
   {
     social: 'github',
-    link: 'itsnitinr',
-    href: 'https://github.com/itsnitinr',
+    link: '"ElPoyo266"',
+    href: 'https://github.com/ElPoyo266',
   },
   {
     social: 'linkedin',
-    link: 'nitinranganath',
-    href: 'https://www.linkedin.com/in/nitinranganath/',
+    link: '"Paul Fournel"',
+    href: 'https://www.linkedin.com/in/paul-fournel-21a60a2a2/',
   },
   {
-    social: 'twitter',
-    link: 'iamnitinr',
-    href: 'https://www.twitter.com/iamnitinr',
-  },
-  {
-    social: 'instagram',
-    link: 'iamnitinr',
-    href: 'https://www.instagram.com/iamnitinr',
-  },
-  {
-    social: 'polywork',
-    link: 'nitinranganath',
-    href: 'https://www.polywork.com/nitinranganath',
+    social: 'discord',
+    link: '"elpoyo"',
+    href: 'https://discord.com/users/elpoyo',
   },
   {
     social: 'telegram',
-    link: 'iamnitinr',
-    href: 'https://t.me/iamnitinr',
-  },
-  {
-    social: 'codepen',
-    link: 'nitinranganath',
-    href: 'https://codepen.io/itsnitinr',
-  },
-  {
-    social: 'codesandbox',
-    link: 'itsnitinr',
-    href: 'https://codesandbox.io/u/itsnitinr',
-  },
+    link: '"paulfournel"',
+    href: 'https://t.me/paulfournel',
+  }
 ];
 
 const ContactCode = () => {
   return (
     <div className={styles.code}>
       <p className={styles.line}>
-        <span className={styles.className}>.socials</span> &#123;
+        <span className={styles.keywordBlue}>public</span>{' '}
+        <span className={styles.keywordBlue}>static</span>{' '}
+        <span className={styles.keywordGreen}>void</span>{' '}
+        <span className={styles.keywordYellow}>contact</span> () &#123;
       </p>
-      {contactItems.slice(0, 8).map((item, index) => (
+      {contactItems.map((item, index) => (
         <p className={styles.line} key={index}>
-          &nbsp;&nbsp;&nbsp;{item.social}:{' '}
-          <a href={item.href} target="_blank" rel="noopener">
-            {item.link}
-          </a>
-          ;
-        </p>
-      ))}
-      {contactItems.slice(8, contactItems.length).map((item, index) => (
-        <p className={styles.line} key={index}>
-          &nbsp;&nbsp;{item.social}:{' '}
+          &nbsp;&nbsp;&nbsp;<span className={styles.keywordBlue}>string</span> <span className={styles.keywordYellow}>{item.social}</span> ={' '}
           <a href={item.href} target="_blank" rel="noopener">
             {item.link}
           </a>
